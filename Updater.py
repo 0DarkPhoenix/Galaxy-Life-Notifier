db3f3e751653ea5d3673130f9d103fb4d039bc03
0Dark_Phoenix_ <sjoerdpeeneman@outlook.com>
2024-05-05 19:24:16 +0200

    Added test file dummy.txt

--- Updater.py
+++ Updater.py
@@ -1,5 +1,7 @@
 import os
+import time
 
+import psutil
 import requests
 
 
@@ -10,14 +12,12 @@ def get_current_version():
     except FileNotFoundError:
         return None
 
-
 def get_latest_version(repo_url):
     response = requests.get(repo_url + "version.txt")
     if response.status_code == 200:
         return response.text.strip()
     else:
         return None
-
 
 def download_new_version(repo_url, filename):
     response = requests.get(repo_url + filename, allow_redirects=True)
@@ -28,9 +28,17 @@ def download_new_version(repo_url, filename):
     return False
 
 
+def kill_process(process_name):
+    """Kill all processes with the given name."""
+    for proc in psutil.process_iter(["pid", "name"]):
+        if proc.info["name"] == process_name:
+            proc.kill()
+
+
 def main():
-    # GitHub repository URL where the exe and version.txt are stored
-    repo_url = "https://raw.githubusercontent.com/0DarkPhoenix/Galaxy-Life-Notifier/v1.2/version.txt"
+    repo_url = (
+        "https://raw.githubusercontent.com/0DarkPhoenix/Galaxy-Life-Notifier/v1.2/"
+    )
 
     current_version = get_current_version()
     latest_version = get_latest_version(repo_url)
@@ -43,8 +51,17 @@ def main():
     print(f"Latest version: {latest_version}")
 
     if latest_version > current_version:
-        print("New version available! Downloading...")
-        exe_filename = "your_program.exe"
+        print(
+            "New version available! Please close the application to proceed with the update."
+        )
+        input("Press ENTER after you have closed the application.")
+
+        exe_filename = "dummy.txt"
+        process_name = "Galaxy Life Notifier.exe"  # The name of the process to kill
+
+        # Optionally kill the process automatically
+        kill_process(process_name)
+
         if download_new_version(repo_url, exe_filename):
             with open("version.txt", "w") as file:
                 file.write(latest_version)
@@ -53,7 +70,6 @@ def main():
             print("Failed to download the new version.")
     else:
         print("You are up-to-date!")
-
 
 if __name__ == "__main__":
     main()
